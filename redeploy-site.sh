8259151f8b89959d5a32bbcc8ccd58c06ca54485
Brandon Josué Magaña Mendoza <79756887+BrandonMagana@users.noreply.github.com>
2022-08-08 16:12:43 -0500

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 #update repo
+cd /root/project-technical-tigers/
 git fetch && git reset origin/main --hard
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d --build
